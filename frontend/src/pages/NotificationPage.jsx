565961416137dff0db19a51ffa7ee8d7329f7832
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-12-30 20:35:34 +0300

    fixed ui in notification and search page

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -2,7 +2,7 @@ import Notification from "../components/Notification"
 
 const NotificationPage = () => {
   return (
-    <div className="h-screen bg-base-100">
+    <div className="min-h-screen bg-base-100">
         <div className="max-w-2xl mx-auto p-4 py-20">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
             <Notification/>  
--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -19,7 +19,7 @@ const SearchPage = () => {
       </div>
     </div>
 
-    <div className="h-full pt-0">
+    <div className="min-h-screen pt-0">
     <div className="max-w-2xl mx-auto p-4 py-8">
       <div className="bg-base-200 rounded-xl p-6 space-y-8">
         {searchResult && searchResult.length > 0 ? <Result/> : <Suggested/>}
